af121249ce6259f2e060354b27f373b9be9ca208
Piyush <159936773+piyushku0331@users.noreply.github.com>
2025-10-21 13:39:10 +0530

    Delete campus-connect-backend directory

--- campus-connect-backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProfile, updateProfile, uploadProfilePicture } = require('../controllers/profileController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/:id', getProfile);
-router.put('/', authMiddleware, uploadProfilePicture, updateProfile);
-
-module.exports = router;
